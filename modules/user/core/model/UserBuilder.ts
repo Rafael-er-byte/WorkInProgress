87e29fd556df0159a6af92775d9b33511d167ff6
Rafael-er-byte <guzmantorresrafaeldejesus@gmail.com>
2025-10-01 21:35:20 -0600

    refactor:UserBuilder and User to throw MissingRequiredParameters erros

--- modules/user/core/model/UserBuilder.ts
+++ modules/user/core/model/UserBuilder.ts
@@ -1,6 +1,6 @@
+import MissingRequiredParameters from "../../../../shared/errors/core/MissingRequiredParameters";
 import type iHasher from "../interfaces/iHasher";
-import User from "./User";
-import BadRequest from "../../../../shared/errors/api/BadRequest";
+import User from "./User";;
 
 export default class UserBuilder {
     userName!: string;
@@ -12,24 +12,24 @@ export default class UserBuilder {
     id!: string;
 
     constructor(public hasher: iHasher, id: string) {
-        if (!hasher || !id) throw new BadRequest('Missing required parameters', { hasher, id });
+        if (!hasher || !id) throw new MissingRequiredParameters('Missing required parameters', { hasher, id });
         this.id = id;
     }
 
     setUserName(userName: string): this {
-        if (!userName) throw new BadRequest('Invalid username', userName);
+        if (!userName) throw new MissingRequiredParameters('Invalid username', userName);
         this.userName = userName;
         return this;
     }
 
     setEmail(email: string): this {
-        if (!email) throw new BadRequest('Invalid email', email);
+        if (!email) throw new MissingRequiredParameters('Invalid email', email);
         this.email = email;
         return this;
     }
 
     setPassword(password: string): this {
-        if (!this.hasher.validate(password)) throw new BadRequest('Invalid password', password);
+        if (!this.hasher.validate(password)) throw new MissingRequiredParameters('Invalid password', password);
         this.password = password;
         return this;
     }
@@ -41,7 +41,7 @@ export default class UserBuilder {
     }
 
     setCreatedAt(date: string): this {
-        if (!date) throw new BadRequest('Invalid date', date);
+        if (!date) throw new MissingRequiredParameters('Invalid date', date);
         this.createdAt = date;
         return this;
     }
@@ -55,8 +55,8 @@ export default class UserBuilder {
         try {
             return new User(this);
         } catch (error) {
-            if (error instanceof BadRequest) throw error;
-            throw new BadRequest('Failed to build user', error);
+            if (error instanceof MissingRequiredParameters) throw error;
+            throw new MissingRequiredParameters('Failed to build user', error);
         }
     }
 };
