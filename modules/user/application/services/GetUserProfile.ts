b2babed556623287f3e791fbb4118ad359f5d6b9
Rafael-er-byte <guzmantorresrafaeldejesus@gmail.com>
2025-10-05 20:03:40 -0600

    Added dto for transport the user id to GetUserProfile service

---
+++ modules/user/application/services/GetUserProfile.ts
@@ -0,0 +1,11 @@
+import type iUserRepository from "../interfaces/repository/iUserRepository";
+
+export default class GetUserProfile{
+    constructor(
+        private repo: iUserRepository
+    ){}
+
+    async execute(){
+        
+    }
+}
