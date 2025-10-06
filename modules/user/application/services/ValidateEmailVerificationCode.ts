e3b2c98c69647dadadda2585242e4ad3fea3ff89
Rafael-er-byte <guzmantorresrafaeldejesus@gmail.com>
2025-10-05 19:42:02 -0600

    building execute function of ValidateEmailVerificationCode class

--- modules/user/application/services/ValidateEmailVerificationCode.ts
+++ modules/user/application/services/ValidateEmailVerificationCode.ts
@@ -1,3 +1,23 @@
+import BadRequest from "../../../../shared/errors/BadRequest";
+import type User from "../../core/model/User";
+import type VerificationCode from "../dtos/in/VerificationCode";
+import type Action from "../dtos/out/Action";
+import type iCodeRepository from "../interfaces/cache/iCodeRepository";
+import type iUserRepository from "../interfaces/repository/iUserRepository";
+
 export default class ValidateEmailVerificationCode{
-    constructor(){}
+    constructor(
+        private repo: iUserRepository,
+        private codeRepo: iCodeRepository
+    ){}
+
+    async execute(code:VerificationCode): Promise<Action>{
+        const userId: string | null = await this.codeRepo.get(`verify:${code.code}`);
+        
+        if(!userId)throw new BadRequest('Invalid code');
+
+        const user: User | undefined = await this.repo.getById(userId);
+
+        user?.verifyEmail();
+    }
 };
