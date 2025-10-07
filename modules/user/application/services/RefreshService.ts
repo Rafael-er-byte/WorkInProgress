abad63bed7ba76bd64a5c308b1d3136d052b2549
Rafael-er-byte <guzmantorresrafaeldejesus@gmail.com>
2025-10-06 21:54:18 -0600

    feat: refresh token service updated to renew tokens

--- modules/user/application/services/RefreshService.ts
+++ modules/user/application/services/RefreshService.ts
@@ -19,14 +19,14 @@ export default class RefreshService{
         const payload:TokenPayLoad = await this.tokenManager.decode(refresh.token);
         if(!payload)throw new Unauthorized('Invalid token');
 
-        const [isTokenValid, currentTTL, user] = await Promise.all([
+        const [isTokenValid, token, user] = await Promise.all([
             this.tokenManager.validate(refresh.token, this.env.token.refresh),
-            this.tokenRepo.getTTL(`refresh:${payload.id}:${refresh.token}`),
+            this.tokenRepo.get(`refresh:${payload.id}:${refresh.token}`),
             this.repo.getById(payload.id)
         ]);
 
         if(!isTokenValid)throw new Unauthorized('Invalid credentials', refresh);
-        if(currentTTL <= 0)throw new Unauthorized('Session expired');
+        if(!token)throw new Unauthorized('Session expired');
         if(!user)throw new Unauthorized('Something went worng');
 
         const [_, newRefreshToken] = await Promise.all([
@@ -34,7 +34,7 @@ export default class RefreshService{
             this.tokenManager.generate(user, this.env.token.refresh, this.env.token.secretRefresh, this.env.token.ttlRefresh)
         ]);
 
-        await this.tokenRepo.set(`refresh:${payload.id}:${newRefreshToken.token}`, newRefreshToken.token, Number(currentTTL));
+        await this.tokenRepo.set(`refresh:${payload.id}:${newRefreshToken.token}`, newRefreshToken.token, this.env.token.ttlRefresh);
         return newRefreshToken;
     }
 };
