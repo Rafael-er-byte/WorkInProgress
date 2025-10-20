41e4f754710a0c9175936caa8bf996f4a50e8c26
Rafael-er-byte <guzmantorresrafaeldejesus@gmail.com>
2025-10-19 21:55:24 -0600

    feat: interface for send messages to broker was added to category module

--- modules/category/application/services/GetCategoryById.ts
+++ modules/category/application/services/GetCategoryById.ts
@@ -9,7 +9,7 @@ export default class GetCategoryById{
 
     async execute(id:string, idCreator:string):Promise<ResponseCategoryDto>{
         if(!this.idManager.validateId(idCreator) || !this.idManager.validateId(id))throw new BadRequest('Invalid data');
-        const category:Category = await this.repo.getById(id, idCreator);
+        const category: Category | undefined = await this.repo.getById(id, idCreator);
         if(!category)throw new BadRequest('Category doesnt exists', id);
         const response: ResponseCategoryDto = new ResponseCategoryDto(category.getName(), category.getIdCreator(), category.getIdCategory());
         return response;
