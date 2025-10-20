6f556c064312feda5eef88e0d072320e396c1b6b
Rafael-er-byte <guzmantorresrafaeldejesus@gmail.com>
2025-10-19 21:12:49 -0600

    fix: update category repository interface to have separated methods to create and update categories

--- modules/category/application/interfaces/iRepository.ts
+++ modules/category/application/interfaces/iRepository.ts
@@ -2,7 +2,8 @@ import type CategoryFilterDto from "../dtos/CategoryFilterDto";
 import type Category from "../../core/model/Category";
 
 export default interface iCategoryRepository{
-    save(category:Category): Promise<void>
+    create(category:Category): Promise<void>
+    update(category:Category): Promise<void>
     getAll(categoryId: Category['idCategory'], idCreator:Category['idCreator'], categoryFilterDto:CategoryFilterDto): Promise<Category[]>
     getById(categoryId: Category['idCategory'], idCreator:Category['idCreator']): Promise<Category | undefined>
     delete(categoryId: Category['idCategory'], idCreator:Category['idCreator']): Promise<boolean>
