98f42b97c65e0ab35cdf69ee3012f01752e68b1d
Rafael-er-byte <guzmantorresrafaeldejesus@gmail.com>
2025-10-19 21:37:28 -0600

    fix: Update SaveCategory service to return an action dto object to give more information

--- modules/category/application/services/SaveCategory.ts
+++ modules/category/application/services/SaveCategory.ts
@@ -4,16 +4,19 @@ import CategoryDto from "../dtos/in/CategryDto";
 import Category from "../../core/model/Category";
 import type iCategoryRepository from "../interfaces/repository/iRepository";
 import BadRequest from "../../../../shared/errors/BadRequest";
+import Action from "../dtos/out/ActionDto";
 
 export default class SaveCategory{
     constructor(private readonly repo: iCategoryRepository, private readonly idManager:IDManager, private readonly dateManager:DateManager){}
 
-    async execute(categoryDto:CategoryDto): Promise<void>{
+    async execute(categoryDto:CategoryDto): Promise<Action>{
         categoryDto.createdAt = this.dateManager.generate();
         categoryDto.idCategory = this.idManager.generateId();
 
         if(!this.idManager.validateId(categoryDto.idCreator))throw new BadRequest('Invalid data');
         const category:Category = new Category(categoryDto.name, categoryDto.idCreator, categoryDto.idCategory, categoryDto.createdAt);
         await this.repo.create(category);
+        const action:Action = new Action(true, categoryDto.idCategory);
+        return action;
     }
 };
