d4eeb92a3c8269f8e0eefcf32a82739005842fe7
Rafael-er-byte <guzmantorresrafaeldejesus@gmail.com>
2025-10-19 21:57:41 -0600

    update: update interface for messagind in category module

---
+++ modules/category/application/interfaces/messaging/iMessenger.ts
@@ -0,0 +1,6 @@
+import type Category from "../../../core/model/Category";
+import type Action from "../../dtos/out/ActionDto";
+
+export default interface iMessenger{
+    saveCategoryLater(category:Category): Promise<Action>
+};
