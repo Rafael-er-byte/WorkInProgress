6a703a8dbfbdb0326c98ac1b0f7731034cd3d057
Rafael-er-byte <guzmantorresrafaeldejesus@gmail.com>
2025-10-20 21:39:31 -0600

    feat: UpdateCategory service was added to Category module

---
+++ modules/category/application/services/UpdateCategry.ts
@@ -0,0 +1,42 @@
+import type IDManager from "../../../shared/contracts/IDManager";
+import CategoryDto from "../dtos/in/CategryDto";
+import Category from "../../core/model/Category";
+import type iCategoryRepository from "../interfaces/repository/iRepository";
+import BadRequest from "../../../../shared/errors/BadRequest";
+import Action from "../dtos/out/ActionDto";
+import type iSearchRepository from "../interfaces/cache/iSearchRepsitory";
+import type iMessenger from "../interfaces/messaging/iMessenger";
+import AppError from "../../../../shared/errors/AppError";
+
+export default class Updateategory{
+    constructor(
+        private readonly repo: iCategoryRepository, 
+        private readonly idManager:IDManager, 
+        private readonly search:iSearchRepository,
+        private readonly messenger: iMessenger
+    ){}
+
+    async execute(categoryDto:CategoryDto): Promise<Action>{
+        if(!this.idManager.validateId(categoryDto.idCreator) && this.idManager.validateId(categoryDto.idCategory))throw new BadRequest('Invalid data', categoryDto);
+        const category:Category = new Category(categoryDto.name, categoryDto.idCreator, categoryDto.idCategory, categoryDto.createdAt as string);
+    
+        let savedOnRepo: boolean = false;
+        let savedOnSearch: boolean = false;
+
+        try {
+            [savedOnRepo, savedOnSearch] = await Promise.all([
+                this.repo.update(category),
+                this.search.update(category)
+            ]);
+        } catch (error) {
+            if(error instanceof AppError && error.code === 500 && savedOnRepo){
+                this.messenger.updateCategoryLater(category);
+            }else throw error;
+        }
+
+        if(!savedOnRepo)throw new AppError('Something went wrong');
+
+        const action:Action = new Action(true, categoryDto.idCategory);
+        return action;
+    }
+};
