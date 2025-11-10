8026aadd98f93a3ba6fb64cd0ccb0896b57c9941
Rafael-er-byte <guzmantorresrafaeldejesus@gmail.com>
2025-11-09 19:44:15 -0600

    refactor: the project structure was refactored changing the name of directories interfaces to contracts and adding directory modules inside each module to after there configure the modules inyeccting dependencies

--- modules/category/api/config/ControllerConfig.ts
+++
@@ -1,7 +0,0 @@
-import type CategoryController from "../controller/CategoryController";
-
-export default class CategoryContrConfig{
-    static createController():CategoryController{
-        
-    }
-};
