ce7dc694bfe9a75bfa453dbfd07f5d23b7ed5024
Rafael-er-byte <guzmantorresrafaeldejesus@gmail.com>
2026-01-07 20:44:36 -0600

    feat: adding NoteContent valueobject and change the Note entitie to be inmmutable

--- src/modules/note/core/model/Note.ts
+++ src/modules/note/core/model/Note.ts
@@ -1,40 +1,29 @@
-import InvalidParameters from "../../../../shared/core/errors/InvalidParameters";
 import Contributor from "../../../../shared/core/objects/Contributor";
 import type DateTime from "../../../../shared/core/objects/DateTime";
 import ID from "../../../../shared/core/objects/ID";
 import Text from "../../../../shared/core/objects/Text";
+import type NoteContent from "../objects/NoteContent";
 
 export default class Note{
-    private static readonly LIMIT_CONTENT_SIZE: number = 223;
 
     private readonly createdAt!:DateTime;
     private readonly id!: ID;
     private readonly creator!:Contributor;
     private readonly taskId!:ID;
-    private lastUpdate!: DateTime;
-    private content!: Text;
+    private content!: NoteContent;
 
     constructor(
         createdAt:DateTime,
         id:ID,
         creator:Contributor,
         taskId:ID,
-        lastUpdate:DateTime,
-        content:Text
+        content:NoteContent
     ){
         this.createdAt = createdAt;
         this.id = id;
         this.creator = creator;
         this.taskId = taskId;
-        this.lastUpdate = lastUpdate;
-        if(content.size() > Note.LIMIT_CONTENT_SIZE)throw new InvalidParameters('Content too large', content);
         this.content = content;
-    }
-
-    public setContent(newContent:Text, now:DateTime):void{
-        if(newContent.size() > Note.LIMIT_CONTENT_SIZE)throw new InvalidParameters('Content too large', newContent);
-        this.content = newContent;
-        this.lastUpdate = now;
     }
 
     public getID():ID{
@@ -42,7 +31,7 @@ export default class Note{
     }
 
     public getContent():Text{
-        return this.content;
+        return this.content.getContent();
     }
 
     public getCreatedAtDate(): DateTime{
@@ -51,10 +40,6 @@ export default class Note{
 
     public getTaskID():ID{
         return this.taskId;
-    }
-
-    public getLastUpdate(): DateTime{
-        return this.lastUpdate;
     }
 
     public getCreator(): Contributor{
---
+++ src/modules/note/core/objects/NoteContent.ts
@@ -0,0 +1,18 @@
+import InvalidParameters from "../../../../shared/core/errors/InvalidParameters";
+import Text from "../../../../shared/core/objects/Text";
+import ValueObject from "../../../../shared/core/objects/ValueObject";
+
+export default class NoteContent extends ValueObject{
+    private content!: Text;
+    private readonly LIMIT_CONTENT_SIZE: number = 223;
+    
+    constructor(content:string){
+        super();
+        this.content = new Text(content);
+        if(this.content.size() > this.LIMIT_CONTENT_SIZE)throw new InvalidParameters('Content too large', content);
+    }
+
+    public getContent():Text{
+        return this.content;
+    }
+}
