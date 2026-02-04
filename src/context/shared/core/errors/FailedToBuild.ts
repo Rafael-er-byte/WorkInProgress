ee4dc89a7dca4eb8c5ee1832119e93aa10ee5ab7
Rafael-er-byte <guzmantorresrafaeldejesus@gmail.com>
2026-02-03 23:12:24 -0600

    delete: unused files

--- src/context/shared/core/errors/FailedToBuild.ts
+++
@@ -1,8 +0,0 @@
-import CoreError from "./CoreError";
-
-export default class FailedToBuild extends CoreError{
-    constructor(message:string, info?:unknown){
-        super(message, info);
-        Object.setPrototypeOf(this, FailedToBuild.prototype);
-    }
-};
--- src/context/shared/core/errors/MissingRequiredParameters.ts
+++
@@ -1,8 +0,0 @@
-import CoreError from "./CoreError";
-
-export default class MissingRequiredParameters extends CoreError{
-    constructor(message:string, missing?:unknown){
-        super(message, missing);
-        Object.setPrototypeOf(this, MissingRequiredParameters.prototype);
-    }
-};
